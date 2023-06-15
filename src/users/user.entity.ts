0eca33b09cafa856ebc628d9bb0fb3aeb6cca762
Chu Văn Giá <gia.chuvan@ncc.asia>
2023-06-15 13:36:32 +0700

    initial commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+   
+  @Column()
+  email: string; 
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Insert User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
