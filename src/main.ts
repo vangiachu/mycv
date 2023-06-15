1e8667024507e9689ece99d372fe4fc98f20c582
Chu Văn Giá <gia.chuvan@ncc.asia>
2023-06-15 17:32:20 +0700

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['chugia'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // chi lay nhung field co trong Dto; nhap them se ko chap nhan!!!
-      whitelist: true
-    })
-  )
   await app.listen(3000);
 }
 bootstrap();
